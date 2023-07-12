fc0bc60055853c4e2c66dc1f2ccf5b7f58cf2783
Anushk Jain <anushk2201@gmail.com>
2023-07-13 01:38:27 +0530

    Routes Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React from 'react';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+// import all components
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+
+// root routes
+const router = createBrowserRouter([
+    {
+        path : '/',
+        element : <Username></Username>
+    },
+    {
+        path : '/register',
+        element : <Register></Register>
+    },
+    {
+        path : '/password',
+        element : <Password></Password>
+    },
+    {
+        path : '/profile',
+        element : <Profile></Profile>
+    },
+    {
+        path : '/recovery',
+        element : <Recovery></Recovery>
+    },
+    {
+        path : '/reset',
+        element : <Reset></Reset>
+    },
+    {
+        path : '/*',
+        element : <PageNotFound></PageNotFound>
+    }
+
+])
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main>
+        <RouterProvider router = {router}></RouterProvider>
+    </main>
   )
 }
 
