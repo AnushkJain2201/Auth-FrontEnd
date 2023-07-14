7b41ee89265a0e73607d2940e1e7b979f960b49a
Anushk Jain <anushk2201@gmail.com>
2023-07-14 23:36:46 +0530

    Password UI Done

--- src/helper/validate.js
+++ src/helper/validate.js
@@ -7,6 +7,31 @@ export async function usernameValidate(values){
     return errors;
 }
 
+export async function passwordValidate(values){
+    const errors = passwordVerify({} , values);
+
+    return errors;
+}
+
+// validate password
+function passwordVerify(error ={} , values){
+    const specialChars = /[$&+,:;=?@#|'<>.-^*()%!]/ ;
+    if(!values.password){
+        error.password = toast.error('Password Required....!');
+    }
+    else if(values.password.includes(" ")){
+        error.password = toast.error('Wrong Password....!');
+    }
+    else if(values.password.length < 4){
+        error.password = toast.error('Password Must Be 4 Characters Long');
+    }
+    else if(!specialChars.test(values.password)){
+        error.password = toast.error('Password Must Have Special Characters');
+    }
+
+    return error;
+}
+
 
 // validate username
 function usernameVerify(error = {} , values){
